34fb3956ac2b122dbe808bfd7f5f0ea84be12bc4
Skyeng <skyeng@MacBook-Air-Admin-2.local>
2023-07-02 19:06:29 +0300

    fix(TaskManager): fix display not in devMode

--- src/components/Widgets/TaskManager/index.tsx
+++ src/components/Widgets/TaskManager/index.tsx
@@ -1,19 +1,25 @@
+import { selectMode } from '../../../store/modeSlice'
+import { useAppSelector } from '../../../hooks'
 import styles from './index.module.scss'
 import DevSkeleton from '../../DevSkeleton'
 
 export const TaskManager = () => {
+  const { devMode } = useAppSelector(state => selectMode(state))
+
   return (
-    <div className={styles.container}>
-      <p className={styles.header}>Task manager / Kanban board</p>
-      <div className={styles.columns}>
-        <DevSkeleton name="Idea" style={{ minHeight: '400px', flex: 1 }} />
-        <DevSkeleton name="Todo" style={{ minHeight: '400px', flex: 1 }} />
-        <DevSkeleton
-          name="In progress"
-          style={{ minHeight: '400px', flex: 1 }}
-        />
-        <DevSkeleton name="Done" style={{ minHeight: '400px', flex: 1 }} />
+    devMode && (
+      <div className={styles.container}>
+        <p className={styles.header}>Task manager / Kanban board</p>
+        <div className={styles.columns}>
+          <DevSkeleton name="Idea" style={{ minHeight: '400px', flex: 1 }} />
+          <DevSkeleton name="Todo" style={{ minHeight: '400px', flex: 1 }} />
+          <DevSkeleton
+            name="In progress"
+            style={{ minHeight: '400px', flex: 1 }}
+          />
+          <DevSkeleton name="Done" style={{ minHeight: '400px', flex: 1 }} />
+        </div>
       </div>
-    </div>
+    )
   )
 }
