f32b9fe6ecebdfc759849ba2f8f85c8352548518
Pirogov Evgenij <pirogovevgenij@gmail.com>
2023-07-12 21:18:03 +0300

    feat(TaskManager): improove board templa2te

--- src/components/Widgets/TaskManager/index.tsx
+++ src/components/Widgets/TaskManager/index.tsx
@@ -1,11 +1,7 @@
-import { selectMode } from '../../../store/modeSlice'
-import { useAppSelector } from '../../../hooks'
 import styles from './index.module.scss'
 import { Board } from './Board'
 
 export const TaskManager = () => {
-  const { devMode } = useAppSelector(state => selectMode(state))
-
   return (
     <div className={styles.container}>
       <p className={styles.widgetHeader}>Task manager / Kanban board</p>
