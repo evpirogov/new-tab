02e930d46b91067dcb3ebbc4701e9f81a8bba6d7
Skyeng <skyeng@MacBook-Air-Admin-2.local>
2023-08-01 18:00:54 +0300

    feat(utils): changeElementPositionInArray

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,1 +1,14 @@
 export const generateRandomId = () => Math.random().toString(32).substring(2)
+
+export const changeElementPositionInArray = <T,>(
+  arr: Array<T>,
+  currentIndex: number,
+  newIndex: number,
+): Array<T> => {
+  if (currentIndex < newIndex) newIndex--
+
+  const element = arr[currentIndex]
+  const newArr = arr.filter((_, i) => i !== currentIndex)
+  newArr.splice(newIndex, 0, element)
+  return newArr
+}
